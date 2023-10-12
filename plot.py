215e5b981bcbaed1e393381f5387c1e6f263da13
silviodonato <silvio.donato@cern.ch>
2023-10-12 19:03:47 +0200

    fixes/improvement. Support years correctly

--- plot.py
+++ plot.py
@@ -1,14 +1,10 @@
 from datasetInfo_fromDAS import summaries
 from recorded_lumi_fromOMS import recorded_lumi
 
-minimalSize = -1E-9 #in GB
+minimalSize = 10000 #in GB
 minEventSize = 0.2E6 # B
 minEraLumi = 1000.0 # pb-1
-useYearInsteadOfEra = False
-
-datasets_ = set()
-eras_ = set()
-formats_ = set()
+useYearInsteadOfEra = True
 
 def datasetNoSplitting(dataset_name):
     if dataset_name[-1].isdigit(): dataset_name = dataset_name[:-1]
@@ -31,6 +27,7 @@ for dataset in list(summaries.keys()):
     if summaries[dataset][0]['file_size']/1E9> minimalSize:
         nn, dataset_name, era, format = dataset.split("/")
         summaries[dataset] = summaries[dataset][0]
+        summaries[dataset]["full_dataset_name"] = dataset_name
         summaries[dataset]["dataset_name"] = datasetNoSplitting(dataset_name)
         era = era.split("-")
         version = era[-1]
@@ -50,16 +47,9 @@ for dataset in list(summaries.keys()):
             summaries[dataset]["delivered_lumi"] = -1
             summaries[dataset]["recorded_lumi"] = -1
             summaries[dataset]["duration"] = -1
-        if useYearInsteadOfEra:
-            era = summaries[dataset]["year"]
-            summaries[dataset]["era"] = era
         summaries[dataset]["version"] = version
         summaries[dataset]["format"] = format
         summaries[dataset]["event_size"] = summaries[dataset]['file_size']/summaries[dataset]['nevents'] if summaries[dataset]['nevents']>0 else 0
-        if summaries[dataset]["event_size"]>minEventSize and int(summaries[dataset]["year"])>=2017 and summaries[dataset]["recorded_lumi"]>minEraLumi:
-            datasets_.add(summaries[dataset]["dataset_name"])
-            eras_.add(era)
-            formats_.add(format)
     else:
         print("Removing %s"%dataset)
         del summaries[dataset]
@@ -67,7 +57,95 @@ for dataset in list(summaries.keys()):
 import pprint
 #pprint.pprint(summaries)
 
+def getDatasetEras(summaries):
+    datasets_ = set()
+    eras_ = set()
+    for dataset in list(summaries.keys()):
+        if summaries[dataset]["event_size"]>minEventSize and int(summaries[dataset]["year"])>=2011 and summaries[dataset]["recorded_lumi"]>minEraLumi:
+            datasets_.add(summaries[dataset]["dataset_name"])
+            eras_.add(summaries[dataset]["era"])
+    return datasets_, eras_
+
+##################################################
+#Add fake dataset
+datasets_, eras_ = getDatasetEras(summaries)
+
+for era in eras_:
+    dataset = "Fake"+era
+    summaries[dataset] = {}
+    summaries[dataset]["eras"] = era
+    summaries[dataset]["year"] = era[3:-1]
+    summaries[dataset]["full_dataset_name"] = "Fake"
+    summaries[dataset]["dataset_name"] = "Fake"
+    summaries[dataset]["format"] = "RAW"
+    summaries[dataset]["event_size"] = 1E6
+    for el in ["file_size", "delivered_lumi", "recorded_lumi", "duration", "nevents", "nlumis", "nblocks", "nfiles", "num_block", "num_lumi"]:
+        summaries[dataset][el] = 0
+    
+    summaries[dataset]["year"] = era[3:-1]
+    if era in recorded_lumi:
+        eraLumi = era
+    elif era[3:] in recorded_lumi:
+        eraLumi = era[3:]
+    else:
+        print(era, " not found in recorded_lumi.")
+        eraLumi = None
+    if eraLumi:
+        summaries[dataset]["delivered_lumi"], summaries[dataset]["recorded_lumi"], summaries[dataset]["duration"] = recorded_lumi[eraLumi][0], recorded_lumi[eraLumi][1], recorded_lumi[eraLumi][2]
+    else:
+        summaries[dataset]["delivered_lumi"] = -1
+        summaries[dataset]["recorded_lumi"] = -1
+        summaries[dataset]["duration"] = -1
+
 ######################################################################
+
+def mergeSummariesInYears(summaries):
+    mergedSummaries = {}
+    for name in summaries:
+        summary = summaries[name]
+        key = (summary["full_dataset_name"], summary["year"], summary["format"])
+        if not key in mergedSummaries.keys(): mergedSummaries[key] = summary.copy()
+        else:
+            for el in ["file_size", "delivered_lumi", "recorded_lumi", "duration", "nevents", "nlumis", "nblocks", "nfiles", "num_block", "num_lumi"]:
+                mergedSummaries[key][el] += summary[el]
+        mergedSummaries[key]["era"] = summary["year"]
+    return mergedSummaries
+
+
+#tot = 0
+#print("CHECK 1")
+#for i in summaries.keys():
+#    if "Tau" in i and "2022" in i: 
+#        print(i, summaries[i]['duration'], summaries[i]['recorded_lumi'], summaries[i]['recorded_lumi']/summaries[i]['duration']*1000)
+#        tot += summaries[i]['duration']
+#print(tot)
+
+if useYearInsteadOfEra:
+    summariesBak = summaries
+    summaries = mergeSummariesInYears(summaries)
+    datasets_, eras_ = getDatasetEras(summaries)
+
+#tot = 0
+#print("CHECK 1")
+#for i in summariesBak.keys():
+#    if "Tau" in i and "2022" in i: 
+#        print(i, summariesBak[i]['duration'], summariesBak[i]['recorded_lumi'], summariesBak[i]['recorded_lumi']/summariesBak[i]['duration']*1000)
+#        tot += summariesBak[i]['duration']
+#print(tot)
+
+#tot = 0
+#print("CHECK 2")
+#for i in summaries.keys():
+#    if "Tau" in i[0] and "2022" in i[1]: 
+#        print(i, summaries[i]['duration'], summaries[i]['recorded_lumi'], summaries[i]['recorded_lumi']/summaries[i]['duration']*1000)
+#        tot += summaries[i]['duration']
+#print(tot)
+
+######################################################################
+
+
+
+##################################################
 
 ## make Canvas using TDR style
 import ROOT
@@ -197,6 +275,7 @@ mergeDataset = {
         "RateTestHI",
         "HeavyFlavor",
         "ScoutingMonitor",
+        "ParkingMonitor"
         
     ],
     "Scouting" : [
@@ -231,6 +310,23 @@ mergeDataset = {
         "ParkingHLTPhysics",
         "ParkingScoutingMonitor",
         "ParkingL1MinimumBias",
+        "DoubleMuParked",
+        "TauParked",
+        "HTMHTParked",
+        "ParkingMuons",
+        "METParked",
+        "ZeroBiasParked",
+        "HLTPhysicsParked",
+        "SinglePhotonParked",
+        "MuOniaParked",
+        "MultiJet1Parked",
+        "ParkingHT450to",
+        "ParkingHT470to",
+        "ParkingHT410to",
+        "ParkingHT430to",
+        "ParkingHT500to",
+        "ParkingHT550to",
+        "VBF1Parked",
     ],
 #    "Parking" : [
 #        "ParkingHLTPhysicsTrains",
@@ -266,21 +362,38 @@ mergeDataset = {
 #        "ParkingScoutingMonitor",
 #        "ParkingL1MinimumBias",
 #    ],
+    "DoubleMuonLowMass" : [
+        "MuOnia",
+        "Charmonium",
+    ],
     "JetMET" : [
         "JetHT",
         "HTMHT",
         "MET",
         "BTagCSV",
+        "HT",
+        "BJetPlusX",
+        "MultiJet"
     ],
+    "Tau" : [
+        "TauPlusX",
+    ],
+    
     "Muon" : [
         "SingleMuon",
         "DoubleMuon",
         "MuonEG",
+        "DoubleMu",
+        "MuHad",
+        "SingleMu"
     ],
     "EGamma" : [
         "SingleElectron",
         "SinglePhoton",
         "DoubleEG",
+        "ElectronHad",
+        "DoublePhoton",
+        "DoubleElectron"
     ],
     "ALCA" : [
         "AlCaLowPtJet",
@@ -367,6 +480,7 @@ mergeDataset = {
         "ParkingHT-Error",
         "CommissioningETM-Error",
         "JetHT-Error",
+        "Fake",
     ],
 }
 
@@ -381,21 +495,25 @@ for dd in mergeDataset.values():
         if not d in check: check.add(d)
         else: raise Exception("Duplicated dataset %s"%d)
 
+notMergeableVariables = ['aveLumi', 'intLumi', 'duration']
+
 def getVariable(summary, var):
+    print(summary)
     if var=='rate_2E34': return summary['nevents']/(summary['recorded_lumi']*1E36/2E34) if summary['recorded_lumi'] else 0
     elif var=='xsect': return summary['nevents']/(summary['recorded_lumi']*1E-3) if summary['recorded_lumi'] else 0
     elif var=='rate': return summary['nevents']/(summary['duration']) if summary['duration'] else 0
     elif var=='data': return summary['file_size']/1E15
     elif var=='dataPerLumi': return summary['file_size']/1E15/(summary['recorded_lumi']/1E3)
+    elif var=='dataPerTime': return summary['file_size']/1E9/(summary['duration'])
     elif var=='events': return summary['nevents']/1E9
     elif var=='aveLumi': 
-        if summary["dataset_name"]=="Tau": return summary['recorded_lumi']*1E36/summary['duration']
+        if summary["dataset_name"]=="Fake": return summary['recorded_lumi']*1E36/summary['duration']
         else: return 0
     elif var=='intLumi': 
-        if summary["dataset_name"]=="Tau": return summary['recorded_lumi']/1E3
+        if summary["dataset_name"]=="Fake": return summary['recorded_lumi']/1E3
         else: return 0
     elif var=='duration': 
-        if summary["dataset_name"]=="Tau": return summary['duration']
+        if summary["dataset_name"]=="Fake": return summary['duration']
         else: return 0
     else: return -1
 
@@ -403,8 +521,9 @@ def getLabel(var):
     if var=='rate_2E34': return "Average trigger rate @2E34cm-1s-1 [Hz]"
     elif var=='xsect': return "Average trigger cross section [fb]"
     elif var=='rate': return "Average trigger rate [Hz]"
-    elif var=='data': return "Total data [PB]"
-    elif var=='dataPerLumi': return "Total data per integ. lumi[PB/fb-1]"
+    elif var=='data': return "Total recorded data [PB]"
+    elif var=='dataPerLumi': return "Recorded data per integ. lumi[PB/fb-1]"
+    elif var=='dataPerTime': return "Average bandwidth during stable beams [GB/s]"
     elif var=='events': return "Total events [B]"
     elif var=='aveLumi': return "Average instantaneous lumi [cm-2s-1]"
     elif var=='intLumi': return "Total integrated lumi [fb-1]"
@@ -423,10 +542,11 @@ def getLabel(var):
 #var = 'data'
 var = 'events'
 #for var in ['xsect', 'rate_2E34', 'rate', 'data', 'events']:
-#for var in ['rate', 'aveLumi', 'intLumi', 'duration', 'xsect', 'rate_2E34', 'rate', 'data', 'events', 'dataPerLumi']:
-for var in ['xsect']:
+for var in ['rate', 'aveLumi', 'intLumi', 'duration', 'xsect', 'rate_2E34', 'rate', 'data', 'events', 'dataPerLumi', 'dataPerTime']:
+#for var in ['aveLumi']:
     eras = sortEras(eras_)
     datasets = sortDatasets(datasets_, summaries)
+    if var in notMergeableVariables: datasets.append("Fake")
 
     canvas = ROOT.TCanvas("canvas", "", 1280, 1024)
     canvas.SetGridx()
@@ -439,9 +559,11 @@ for var in ['xsect']:
 
     ## fill histograms
     for summary in summaries.values():
-        if summary['dataset_name'] in datasets and summary['era'] in eras:
-            dataset_sizes[summary['dataset_name']].Fill( eras.index(summary['era']), getVariable(summary, var)  ) #/ summary['num_lumi']
-            print(summary['dataset_name'], summary['era'], summary['file_size'])
+        datName = summary['dataset_name']
+        if datName in datasets and summary['era'] in eras:
+            dataset_sizes[datName].Fill( eras.index(summary['era']), getVariable(summary, var)  ) #/ summary['num_lumi']
+            dataset_sizes[datName].SetBinError( eras.index(summary['era'])+1, 0.0001 )
+            print(datName, summary['era'], summary['file_size'])
 
     for merge in mergeDataset:
         if not merge in dataset_sizes:
@@ -451,8 +573,8 @@ for var in ['xsect']:
             if tobemerged in dataset_sizes:
                 dataset_sizes[merge].Add(dataset_sizes[tobemerged])
                 if tobemerged in datasets: datasets.remove(tobemerged)
-
-    if "ToBeRemoved" in datasets: datasets.remove("ToBeRemoved")
+    
+        if "ToBeRemoved" in datasets: datasets.remove("ToBeRemoved")
 
     ##sort by size
     print("________________________________")
@@ -477,12 +599,12 @@ for var in ['xsect']:
         d = datasets[i]
         dataset_sizes[d].SetLineColor(ROOT.kBlack)
         dataset_sizes[d].SetFillColor(colors[i%len(colors)])
-        stack.Add(dataset_sizes[d])
+        if dataset_sizes[d].Integral()>0: stack.Add(dataset_sizes[d])
 
     for i in range(len(datasets)):
         d = datasets[len(datasets)-1-i]
         print(d)
-        leg.AddEntry(dataset_sizes[d],d) # or lep or f
+        if dataset_sizes[d].Integral()>0: leg.AddEntry(dataset_sizes[d],d) # or lep or f
 
     #stack.Add("Others")
 
@@ -491,11 +613,13 @@ for var in ['xsect']:
     #    canvas.Update()
 
     stack.Draw("HIST")
+#    if var!='aveLumi': stack.Draw("HIST")
+#    else:  stack.Draw("E0")
     stack.SetMaximum(stack.GetMaximum()*1.6)
     stack.GetXaxis().SetTitle("Eras")
     stack.GetYaxis().SetTitle(getLabel(var))
-    leg.Draw()
-
+    if not (var in notMergeableVariables): leg.Draw() 
+    
     canvas.SaveAs("plot_"+var+".png")
 
 
